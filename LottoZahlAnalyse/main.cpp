62b549e638f7e65bf00fc9c6045ea4c37fb88349
SCALL <info@callsen.org>
2018-11-17 08:37:33 +0100

    Initial Commit

---
+++ LottoZahlAnalyse/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  LottoZahlAnalyse
+//
+//  Created by Stephan on 17.11.18.
+//  Copyright © 2018 Stephan Callsen. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
